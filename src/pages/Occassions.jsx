4e40db8249753d2b0ffafc581570a75b77d09789
Elsa George <elsageorge123@gmail.com>
2024-09-14 22:59:00 +0530

    sidebar

--- src/pages/Occassions.jsx
+++ src/pages/Occassions.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import { christmas, easter, halloween } from '../utils/images';
 
 
 function Occassions() {
   const data = [
     {
       event: 'Christmas',
-      img: 'src/images/christmas.png',
+      img: christmas,
     },
     {
-      event: 'Onam',
-      img: ''
+      event: 'Easter',
+      img: easter,
     },
     {
-      event: 'Baptism',
-      img: ''
+      event: 'Halloween',
+      img: halloween,
     },
     {
       event: 'Baby Shower',
@@ -28,29 +29,30 @@ function Occassions() {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
   }
 
   return (
-    <div className='w-3/4 m-auto'>
-      <div className='mt-20'>
-        <Slider {...settings}>
+    <>
+      <div className='w-3/4 m-auto'>
+        <div className='mt-20'>
+          <Slider {...settings}>
+            {data.map((d) => (
+              <div className="bg-white h-[450px] text-black rounded-xl">
+                <div style={{ backgroundImage: `url(${d.img})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPositionX: 'center' }} className='h-80 rounded-t-xl bg-white flex justify-center items-center'>
 
-          {data.map((d) => (
-            <div className="bg-white h-[450px] text-black rounded-xl">
-              <div className='h-56 rounded-t-xl bg-indigo-500 flex justify-center items-center'>
-                <img src={d.img} alt="" className='h-44 w-44 rounded-full' />
+                </div>
+                <div className='flex flex-col items-center justify-center gap-4'>
+                  <p className='text-xl font-semibold'>{d.event}</p>
+                  <buttom className="bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl">Customise Bingo</buttom>
+                </div>
               </div>
-              <div className='flex flex-col items-center justify-center gap-4'>
-                <p className='text-xl font-semibold'>{d.event}</p>
-                <buttom className="bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl">Customise Bingo</buttom>
-              </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
+
       </div>
-
-    </div>
+    </>
   );
 }
 
